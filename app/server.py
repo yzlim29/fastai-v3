85d5a5e93998f70e8b8e8890638e3c0d50519ee1
yzlim29 <82995323+yzlim29@users.noreply.github.com>
2021-04-22 13:52:31 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = "https://www.googleapis.com/drive/v3/files/1g6o_k48kdJFyCT0TGPb8jOpDZjIOleDC?alt=media&key=AIzaSyC2HJLf-evjJFjgt5yXonloX37jVtUtidU"
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bulbasaur', 'charmander', 'pikachu','squirtle']
 path = Path(__file__).parent
 
 app = Starlette()
